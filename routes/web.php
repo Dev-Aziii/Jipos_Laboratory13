8d5e4a548f5a91d8d554bbaaf8eb9c25bffdecee
dev_azi <azi26.dev@gmail.com>
2025-04-21 10:59:34 +0800

    commit

--- routes/web.php
+++ routes/web.php
@@ -1,21 +1,21 @@
 <?php
 
-use App\Http\Controllers\UserController;
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('auth.login');
 });
 
-Route::get('/register', [UserController::class, 'showRegister'])->name('showRegister');
-Route::post('/register', [UserController::class, 'register'])->name(name: 'registration.post');
+Route::get('/register', [AuthController::class, 'showRegister'])->name('showRegister');
+Route::post('/register', [AuthController::class, 'register'])->name(name: 'registration.post');
 
 
-Route::get('/login', [UserController::class, 'showLogin'])->name('showLogin');
-Route::post('/login', [UserController::class, 'login'])->name(name: 'login.post');
+Route::get('/login', [AuthController::class, 'showLogin'])->name('showLogin');
+Route::post('/login', [AuthController::class, 'login'])->name(name: 'login.post');
 
 
-Route::post('/logout', [UserController::class, 'logout'])->middleware('auth');
+Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth');
 
 
 Route::get('/dashboard', function () {
